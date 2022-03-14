3984a23baee95d7864f0477c5b2c6d113a9a43eb
pepper90 <panayotovj@gmail.com>
2022-03-14 11:37:40 +0200

    Added location permissions

--- app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeBindingAdapters.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeBindingAdapters.kt
@@ -41,18 +41,20 @@ fun fetchImage(view: ImageView, src: String?) {
     }
 }
 
-@BindingAdapter("stateValue")
-fun Spinner.setNewValue(value: String?) {
-    val adapter = toTypedAdapter<String>(this.adapter as ArrayAdapter<*>)
-    val position = when (adapter.getItem(0)) {
-        is String -> adapter.getPosition(value)
-        else -> this.selectedItemPosition
-    }
-    if (position >= 0) {
-        setSelection(position)
-    }
-}
+// Not needed because we are using AutoCompleteTextView
 
-inline fun <reified T> toTypedAdapter(adapter: ArrayAdapter<*>): ArrayAdapter<T>{
-    return adapter as ArrayAdapter<T>
-}
+//@BindingAdapter("stateValue")
+//fun Spinner.setNewValue(value: String?) {
+//    val adapter = toTypedAdapter<String>(this.adapter as ArrayAdapter<*>)
+//    val position = when (adapter.getItem(0)) {
+//        is String -> adapter.getPosition(value)
+//        else -> this.selectedItemPosition
+//    }
+//    if (position >= 0) {
+//        setSelection(position)
+//    }
+//}
+
+//inline fun <reified T> toTypedAdapter(adapter: ArrayAdapter<*>): ArrayAdapter<T>{
+//    return adapter as ArrayAdapter<T>
+//}
